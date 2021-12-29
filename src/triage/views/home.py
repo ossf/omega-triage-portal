f93f85246e6ce023b6d11c17d99a663bd51eb18d
Michael Scovetta <michael.scovetta@microsoft.com>
2021-12-29 08:06:08 +0000

    Clean up home page

--- src/triage/views/home.py
+++ src/triage/views/home.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import (
     HttpRequest,
     HttpResponse,
@@ -7,6 +6,13 @@ from django.http import (
     HttpResponseRedirect,
     JsonResponse,
 )
+from django.shortcuts import render
+
+from triage.models import Case, Finding
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'triage/home.html', {})
+    finding_last_updated = Finding.objects.all().order_by("-updated_at").first().created_at
+    case_last_updated = Case.objects.all().order_by("-updated_at").first().created_at
+    context = {"finding_last_updated": finding_last_updated, "case_last_updated": case_last_updated}
+    return render(request, "triage/home.html", context)
