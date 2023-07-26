1f5591153377259bb8426c6b5653052e6dc18511
glenda1015 <glendag1015@gmail.com>
2023-07-26 17:27:06 -0400

    Rolling back changes to debug pylint error
    Signed-off-by: glenda1015 <glendag1015@gmail.com>

--- src/triage/urls.py
+++ src/triage/urls.py
@@ -1,8 +1,6 @@
 """This module URL patterns specific to the Triage Portal."""
 
 from django.urls import path
-from graphene_file_upload.django import FileUploadGraphQLView
-from triage.api.schema import schema
 from triage.views import attachments, cases, filters, findings, home, tool_defect, wiki
 
 urlpatterns = [
@@ -51,6 +49,4 @@ urlpatterns = [
     path("wiki/", wiki.home),
     # Default (Home)
     path("", home.home),
-    # GraphQl Url
-    path("graphql", FileUploadGraphQLView.as_view(graphiql=True, schema=schema)),
 ]
